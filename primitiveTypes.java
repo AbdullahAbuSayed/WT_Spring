10666c2ca3dd0c6dfc14e9fff35206974ccb4cae
Abdullah Abu Sayed <Abdullahabusayedrafi@gmail.com>
2026-02-16 08:41:05 -0200

    first try

---
+++ primitiveTypes.java
@@ -0,0 +1,46 @@
+public class primitiveTypes {
+public static void main(string[]args) {
+byte age = 25;
+short year = 2024;
+int a = 100000;
+long population = 7800000000;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
